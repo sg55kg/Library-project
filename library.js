3ac0033710c8358efa25838fa7077c0c5994e863
sg55kg <87102016+sg55kg@users.noreply.github.com>
2021-08-11 22:29:27 -0500

    Add files via upload

---
+++ library.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function addBookToLibrary() {
+        addBookButton = document.getElementBy("addBook");
+        addBookButton.addEventListener('click', function() {
+
+        let getTitle = document.getElementBy("getTitle").value;
+        let getAuthor = document.getElementBy("getAuthor").value;
+        let getInfo = document.getElementBy("getInfo").value;
+
+        
+
+     });
+}
