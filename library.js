f6b641b6bb91a4c9a403d9aa4005e7df4ad23d56
Sam Dollar <sdollar1@gmail.com>
2021-08-12 18:41:37 -0500

    added a div for submitting new books that can be hidden/revealed on button click

--- library.js
+++ library.js
@@ -1,6 +1,15 @@
-let addButton = document.getElementById('add-book');
+let addButton = document.getElementById('open-add-div');
+let addNewDiv = document.getElementById('add-new');
+let submitBook = document.getElementById('submit-new-book');
 let myLibrary = [];
 
+addButton.addEventListener('click', function() {
+    addNewDiv.style.display = 'block';
+})
+
+submitBook.addEventListener('click', function() {
+    addNewDiv.style.display = 'none';
+})
 
 function addBook() {
     
