fc167eaf1af195c72ab04d7bc751a9e1c1ee9135
Sam Dollar <sdollar1@gmail.com>
2021-08-15 20:56:27 -0500

    fixed js & updated README

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 Odin Library Project
 
 Update(8/15/21):
-More javascript today. Now the new book information is displayed on the page, but it does not inherit the book div's style. Next up will be fixing that and then adding a server to save user inputted books when the page reloads.
+    More javascript today. Now the new book information is displayed on the page, but it does not inherit the book div's style. Next up will be fixing that and then adding a server to save user inputted books when the page reloads.
+
+    Second update: Fixed the issue earlier, so now when a new book is added it appears in it's own grey box like the test example. I also watched some videos on modular js ( https://www.youtube.com/watch?v=m-NYyst_tiY&list=PLoYCgNOIyGABs-wDaaxChu82q_xQgUb4f&index=2 ) and reworked how the code was written. It's a little cleaner and easier to follow now too. 
+
+    Now I just need to get the new book divs to display in a nicer grid format or something so they don't just keep going down the page in a single column.
 
 Update(8/14/21):
-Worked more on the javascript. Now it takes user input and makes a new book which is pushed into the MyLibrary array, and right now the array is just console logged. 
+    Worked more on the javascript. Now it takes user input and makes a new book which is pushed into the MyLibrary array, and right now the array is just console logged. 
 
-Next will be displaying this information on the page and creating a new book div every time a new book is submitted. I also would like to add a remove book function. Also futher down the line I'd like to add storage so each person's library will be saved when the page is reloaded or a log in system. Still a lot to do and pretty overwhelming, but progress is being made.
+    Next will be displaying this information on the page and creating a new book div every time a new book is submitted. I also would like to add a remove book function. Also futher down the line I'd like to add storage so each person's library will be saved when the page is reloaded or a log in system. Still a lot to do and pretty overwhelming, but progress is being made.
 
 Update(8/13/21):
-Using flexbox to style the page. I think I'll be ready to start working more on the javascript later today.
+    Using flexbox to style the page. I think I'll be ready to start working more on the javascript later today.
 
 Update(8/12/21):
-This probably won't be the only update today, but so far I've just continued laying some foundations and figuring out where everything will go on the page. Added working buttons that hide the add-new book div that will disappear when a new book is submitted. This project has a lot of different elements I need to figure out, so just trying to take it one step at a time.
+    This probably won't be the only update today, but so far I've just continued laying some foundations and figuring out where everything will go on the page. Added working buttons that hide the add-new book div that will disappear when a new book is submitted. This project has a lot of different elements I need to figure out, so just trying to take it one step at a time.
 
 Update(8/11/21):
-Getting everything organized and laying out the foundation.
+    Getting everything organized and laying out the foundation.
--- library.js
+++ library.js
@@ -1,65 +1,70 @@
-let addButton = document.getElementById('open-add-div');
-let addNewDiv = document.getElementById('add-new');
-let submitBook = document.getElementById('submit-new-book');
-let bookList = document.getElementById('book-list');
-//let inputField = document.querySelector('.input');
-let getTitle = document.getElementById('title');
-let getAuthor = document.getElementById('author');
-let getPages = document.getElementById('pages');
-let getReadStatus = document.getElementById('read-status');
 
-let myLibrary = [];
+(function () {
+   let myLibrary = {
+      library: [],
+      init: function() {
+         this.cacheDom();
+         this.bindEvents();
+      },
+      cacheDom: function() {
+         this.addButton = document.getElementById('open-add-div');
+         this.addNewDiv = document.getElementById('add-new');
+         this.submitBook = document.getElementById('submit-new-book');
+         this.bookList = document.getElementById('book-list');
+         this.getTitle = document.getElementById('title');
+         this.getAuthor = document.getElementById('author');
+         this.getPages = document.getElementById('pages');
+         this.getReadStatus = document.getElementById('read-status');
+      },
+      displayAddDiv: function() {
+         this.addNewDiv.style.display = 'flex';
+      },
+      addBook: function() {
+         this.addNewDiv.style.display = 'none';
+         this.getBookInfo();
+      },
+      bindEvents: function() {
+         this.addButton.addEventListener('click', this.displayAddDiv.bind(this));
+         this.submitBook.addEventListener('click', this.addBook.bind(this));
+      },
+      getBookInfo: function() {
+         let book = function(title, author, pages, readStatus) {
+         this.title = title;
+         this.author = author;
+         this.pages = pages;
+         this.readStatus = readStatus;
+         }
+         let bookDiv = document.createElement('div');
+         bookDiv.classList.add('book');
+         this.bookList.appendChild(bookDiv);
 
-addButton.addEventListener('click', function() {
-    addNewDiv.style.display = 'flex';
-    // need to clear the input fields when this loads
+         const title = this.getTitle.value;
+         let titleNode = document.createElement('p');
+         titleNode.innerText = `Title: ${title}`;
 
-})
+         const author = this.getAuthor.value;
+         let authorNode = document.createElement('p');
+         authorNode.innerText = `Author: ${author}`;
 
-submitBook.addEventListener('click', function() {
-    addNewDiv.style.display = 'none';
-    getBookInfo();
-    console.log(myLibrary);
-})
+         const pages = this.getPages.value;
+         let pageNode = document.createElement('p');
+         pageNode.innerText = `Pages: ${pages}`;
+
+         const readStatus = this.getReadStatus.checked;
+         let readNode = document.createElement('p');
+         readNode.innerText = `Read status: ${readStatus}`;
+
+         let newBook = new book(title, author, pages, readStatus);
+         this.library.push(newBook);
+
+         bookDiv.appendChild(titleNode);
+         bookDiv.appendChild(authorNode);
+         bookDiv.appendChild(pageNode);
+         bookDiv.appendChild(readNode);
+      },
+   }
+   myLibrary.init();
+})();
 
 
-function book(title, author, pages, readStatus) {
-    
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.readStatus = readStatus;
-    
-}
-
-const getBookInfo = () => {
-    let bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-    bookList.appendChild(bookDiv);
-
-    const title = getTitle.value;
-    let titleNode = document.createElement('p');
-    titleNode.innerText = `Title: ${title}`;
-
-    const author = getAuthor.value;
-    let authorNode = document.createElement('p');
-    authorNode.innerText = `Author: ${author}`;
-
-    const pages = getPages.value;
-    let pageNode = document.createElement('p');
-    pageNode.innerText = `Pages: ${pages}`;
-
-    const readStatus = getReadStatus.checked;
-    let readNode = document.createElement('p');
-    readNode.innerText = `Read status: ${readStatus}`;
-
-    let newBook = new book(title, author, pages, readStatus);
-    myLibrary.push(newBook);
-
-    bookDiv.appendChild(titleNode);
-    bookDiv.appendChild(authorNode);
-    bookDiv.appendChild(pageNode);
-    bookDiv.appendChild(readNode);
-
-}
 
