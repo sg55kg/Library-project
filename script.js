44c5f68abcaefe8dd928fcfbc58a92301801ec64
Sam Dollar <sdollar1@gmail.com>
2021-08-24 17:29:41 -0500

    display new book

--- script.js
+++ script.js
@@ -17,12 +17,8 @@ class Book {
     }
 
     toggleRead(e) {
-        if(this.readStatus == true) {
-            this.readStatus = false;
-        } else {
-            this.readStatus = true;
-        }
-    }
+        console.log("test");
+    } //readBtn.addEventListener('click', toggleRead.bind(this)); (maybe insert somewhere in here)
 }
 
 
@@ -55,16 +51,55 @@ function openNewBookForm() {
 function submitNewBook() {
     newBookForm.style.display = 'none';
     overlay.classList.remove('active');
-
+    displayNewBook();
+    addBookToLibrary();
     
+}
+
+
+function displayNewBook() {
+    let bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookList.appendChild(bookDiv);
+
+    const title = titleInput.value;
+    let titleNode = document.createElement('p');
+    titleNode.innerText = `Title: ${title}`;
+
+    const author = authorInput.value;
+    let authorNode = document.createElement('p');
+    authorNode.innerText = `Author: ${author}`;
+
+    const pages = pagesInput.value;
+    let pageNode = document.createElement('p');
+    pageNode.innerText = `Pages: ${pages}`;
+
+    const status = getReadStatus.checked;
+    let readNode = document.createElement('p');
+    readNode.classList.add('book-status');
+    readNode.innerText = `Read status: ${status}`;
+
+    let readBtn = document.createElement('button');
+    readBtn.classList.add('toggle-read');
+    readBtn.innerText = 'Toggle Read';
+    readBtn.addEventListener('click', toggleRead.bind(this));
+
+    let newBook = new Book(title, author, pages, status);
+    library.push(newBook);
+
+    bookDiv.appendChild(titleNode);
+    bookDiv.appendChild(authorNode);
+    bookDiv.appendChild(pageNode);
+    bookDiv.appendChild(readNode);
+    bookDiv.appendChild(readBtn);
+
 }
 
 
 function addBookToLibrary() {
 
-}
 
-//cacheDom();
+}
 
 
 newBookFormBtn.addEventListener('click', openNewBookForm);
